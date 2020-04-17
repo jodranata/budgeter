e6f2dad1248afb0b90705df19c7d0304d4caf5b1
Jodi Pranata <jodranata@gmail.com>
2020-04-17 23:24:32 +0700

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+const hello = 'Hellow';
+function raw() {
+  const data = 'data';
+  const is = 'is';
+
+  if (hello === 'hellow') {
+    return console.log('hello');
+  }
+}
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
