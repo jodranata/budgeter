8508efa0bb3590a88ed7c71a87a0a75e68bdd456
Jodi Pranata <jodranata@gmail.com>
2020-04-29 21:11:44 +0700

    Updates

---
+++ precache-manifest.c78cd35df8720f036008469a2d50b57f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2107d8480590f5f3b073ef61763b9ba",
+    "url": "/budgeter/index.html"
+  },
+  {
+    "revision": "bd2f0493e64dafefed13",
+    "url": "/budgeter/static/css/main.7dd9749a.chunk.css"
+  },
+  {
+    "revision": "dab8b89262272e76834f",
+    "url": "/budgeter/static/js/2.6fa59831.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/budgeter/static/js/2.6fa59831.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd2f0493e64dafefed13",
+    "url": "/budgeter/static/js/main.194ee0dd.chunk.js"
+  },
+  {
+    "revision": "c6c2075db85f4a42786a",
+    "url": "/budgeter/static/js/runtime-main.309c1224.js"
+  }
+]);
